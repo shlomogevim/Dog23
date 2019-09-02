b0aba79f19ba48f1f4fcd7f76f88b50fcf58eeb5
shlomogevim <shlomo.gevim@gmail.com>
2019-09-02 13:01:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dog23A/model/DogApi.kt
@@ -0,0 +1,9 @@
+package com.example.dog23A.model
+
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface DogApi {
+    @GET("DevTides/DogsApi/master/dogs.json")
+    fun getDogs():Single<List<DogBreed>>
+}
